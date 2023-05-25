cd208c76c31fdbae6abf089b2d3ad47e1085fe2a
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-05-25 20:44:19 +0530

    Implemented input section areas

---
+++ components/locationdate.tsx
@@ -0,0 +1,19 @@
+export function Locationdate({ isSwitch }: { isSwitch?: boolean }) {
+  const inputClassName = `block p-4 pl-10 mx-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-400 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black ${
+    isSwitch ? "w-10" : "w-50"
+  }`
+
+  return (
+    <div className="flex">
+      <input
+        type="search"
+        id="default-search"
+        className={inputClassName}
+        placeholder=""
+        required
+      />
+    </div>
+  )
+}
+
+export default Locationdate
---
+++ config/locationdate.ts
@@ -0,0 +1,18 @@
+export const LocationDateConfig = [
+  {
+    name: "from",
+  },
+  {
+    name: "switch",
+    isSwitch: "true",
+  },
+  {
+    name: "to",
+  },
+  {
+    name: "fromdate",
+  },
+  {
+    name: "todate",
+  },
+]
