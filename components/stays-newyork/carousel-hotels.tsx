3746cf7ce23fa816f67cf491aa3fd43faa949108
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-06-02 13:01:03 +0530

    Implemented Math.random() function to select random props to display as hotels

--- components/stays-newyork/carousel-hotels.tsx
+++ components/stays-newyork/carousel-hotels.tsx
@@ -3,9 +3,16 @@ import { NEWYORK_MOCK_LOCATIONS } from "@/config/hotelnewyorkdata"
 import { NewYorkStays } from "./hotel-stays-newyork"
 
 export function HotelsNewYork({}) {
+  const randomIndex = Math.floor(Math.random() * NEWYORK_MOCK_LOCATIONS.length)
+  const randomLocation = NEWYORK_MOCK_LOCATIONS[randomIndex]
+  const otherLocations = NEWYORK_MOCK_LOCATIONS.filter(
+    (location, index) => index !== randomIndex
+  )
+
   return (
     <div className="p-3 flex">
-      {NEWYORK_MOCK_LOCATIONS.map((location) => (
+      <NewYorkStays location={randomLocation} />
+      {otherLocations.map((location) => (
         <NewYorkStays location={location} />
       ))}
     </div>
